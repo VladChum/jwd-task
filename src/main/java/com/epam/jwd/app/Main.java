2b0ed85a344036688c4426490464fb2606a101ee
Vlad Chumachov <DegaDega001@mail.ru>
2021-02-03 22:49:30 +0300

    fix bag

--- src/main/java/com/epam/jwd/app/Main.java
+++ src/main/java/com/epam/jwd/app/Main.java
@@ -13,20 +13,35 @@ public class Main {
     public static Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws FigureException {
-        List<Point> pointsForLine = new ArrayList<>();
-        pointsForLine.add(new Point(1, 1));
-        pointsForLine.add(new Point(2, 2));
-        pointsForLine.add(new Point(1, 1));
-        pointsForLine.add(new Point(2, 2));
+        List<Point> pointsForMultiAngel = new ArrayList<>();
+        pointsForMultiAngel.add(new Point(1, 1));
+        pointsForMultiAngel.add(new Point(4, 1));
+        pointsForMultiAngel.add(new Point(4, 4));
+        pointsForMultiAngel.add(new Point(1, 4));
 
         List<Point> pointsForTriangle = new ArrayList<>();
         pointsForTriangle.add(new Point(1, 1));
         pointsForTriangle.add(new Point(1, 5));
         pointsForTriangle.add(new Point(4, 1));
 
-        Figure triangle = new FigureFactory().createFigure(FigureType.TRIANGLE, pointsForTriangle);
-        triangle.outputInformation();
-        logger.log(Level.INFO, triangle.calculateArea());
-        logger.log(Level.INFO, triangle.calculatePerimeter());
+        try {
+            Figure multiAngeleFigure = new FigureFactory().createFigure(FigureType.MULTIANGLE, pointsForMultiAngel);
+            multiAngeleFigure.outputInformation();
+            logger.log(Level.INFO, multiAngeleFigure.calculateArea());
+            logger.log(Level.INFO, multiAngeleFigure.calculatePerimeter());
+        } catch (FigureException e) {
+            logger.log(Level.ERROR, "Exception " + e.toString());
+        }
+
+        try {
+            Figure triangle = new FigureFactory().createFigure(FigureType.TRIANGLE, pointsForTriangle);
+            triangle.outputInformation();
+            logger.log(Level.INFO, triangle.calculateArea());
+            logger.log(Level.INFO, triangle.calculatePerimeter());
+        } catch (FigureException e) {
+            logger.log(Level.ERROR, "Exception " + e.toString());
+
+        }
+
     }
 }
