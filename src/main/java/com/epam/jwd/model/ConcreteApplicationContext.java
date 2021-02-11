fead395e2c7711429e6baa9d3073ef798bcc5a44
Vlad Chumachov <DegaDega001@mail.ru>
2021-02-11 23:41:57 +0300

    add figureCash

--- src/main/java/com/epam/jwd/model/ConcreteApplicationContext.java
+++ src/main/java/com/epam/jwd/model/ConcreteApplicationContext.java
@@ -1,15 +1,33 @@
 package com.epam.jwd.model;
 
+import com.epam.jwd.decorator.PostProcessorCashDecorator;
 import com.epam.jwd.factory.ApplicationContext;
 import com.epam.jwd.factory.Factory;
 import com.epam.jwd.decorator.LogPostProcessorDecorator;
 import com.epam.jwd.decorator.PostProcessorDecorator;
 import com.epam.jwd.decorator.PreProcessorDecorator;
+import org.apache.log4j.Level;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.epam.jwd.app.Main.logger;
 
 public class ConcreteApplicationContext implements ApplicationContext {
+    private static List<Figure> figureCash = new ArrayList<>();
+
+    public void setFigureCash(Figure figure) {
+        this.figureCash.add(figure);
+    }
+
+    @Override
+    public void outputFigureCash() {
+        logger.log(Level.INFO, "Coll figure in figureCash :" + figureCash.size());
+    }
+
     @Override
     public Factory createFigureFactory() {
-        return new LogPostProcessorDecorator(new PostProcessorDecorator(
-                new PreProcessorDecorator(new FigureFactory())));
+        return new LogPostProcessorDecorator(new PostProcessorCashDecorator(
+                new PostProcessorDecorator(new PreProcessorDecorator(new FigureFactory()))));
     }
 }
